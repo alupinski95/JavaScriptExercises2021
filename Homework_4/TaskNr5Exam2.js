8f7598c543b60eda842148838dacf51d93779c2a
Arkadiusz Łupiński <a.lupinski@gmail.com>
2021-11-12 07:28:39 +0100

    Exam task started

--- Homework_4/TaskNr5Exam2.js
+++ Homework_4/TaskNr5Exam2.js
@@ -1,30 +1,77 @@
 // losujesz 5 kart i wykrywasz najwyższy ukłąd
 // https://m.media-amazon.com/images/I/71xmLOiiywL._AC_SY741_.jpg
-let pickedCards = [];
-let arrayCheckIsPicked = Array.from(Array(14), () => new Array(5));
-console.info(arrayCheckIsPicked);
-const colorByValue = ['heart','diamond','club','spade']
+
+const colorByValue = ['heart', 'diamond', 'club', 'spade']
+
 function getRandomIndex(max) {
-    return Math.floor(Math.random() * (max ))+1;
+    return Math.floor(Math.random() * (max)) + 1;
 }
-function pickFiveRandomCards(cardToPick){
-    for(;cardToPick;){
-        let val = getRandomIndex(13),col = getRandomIndex(4); //col = colorByValue[getRandomIndex(3)];
-        let newCard = { 
-            value:val,
+
+function pickFiveRandomCards(cardToPick) {
+    let pickedCards = []
+    let arrayCheckIsPicked = Array.from(Array(14), () => new Array(5));
+    for (; cardToPick;) {
+        let val = getRandomIndex(13), col = getRandomIndex(4);
+        let newCard = {
+            value: val,
             color: col
         }
-        if(arrayCheckIsPicked || !arrayCheckIsPicked[val][col])
-        {
+        if (arrayCheckIsPicked || !arrayCheckIsPicked[val][col]) {
             pickedCards.push(newCard);
             arrayCheckIsPicked[val][col] = 'x';
-            cardToPick-=1;
+            cardToPick -= 1;
         }
     }
+    return pickedCards;
 }
-function main(){
-    pickFiveRandomCards(5);
-
+function checkHand(cards) {
+    let sorted = cards.sort(compareObjects);
+    let nameOfHand = chechIsPokerOrStrit(sorted);
+    if (!nameOfHand)
+        nameOfHand = chechIsPairThreeOrFour(sorted);
+    if (!nameOfHand)
+        nameOfHand = checkIsHighOrFlush(sorted);
+    return nameOfHand;
 }
 
+function compareObjects(a, b) {
+    return a.value - b.value;
+}
+
+function main() {
+    let pickedCards = pickFiveRandomCards(5);
+    checkHand(pickedCards);
+}
+function isColorSame(sumofColor) {
+    return Math.floor(sumofColor / 4) == 4 ||
+        Math.floor(sumofColor / 3) == 4 ||
+        Math.floor(sumofColor / 2) == 4 ||
+        Math.floor(sumofColor / 1) == 4;
+}
+function chechIsPokerOrStrit(cards) {
+    let pattern = '1,2,3,4,5,6,7,8,9,10,11,12,13,1';
+    let handPattern = '';
+    let sumOfColors = 0;
+
+    cards.forEach(element => {
+        handPattern += element.value.toString() + ',';
+        sumOfColors += element.color;
+    });
+
+    if (handPattern.includes('10,11,12,13') &&
+        handPattern.includes('0')) {
+        return (isColorSame(sumOfColors)) ? "Royal flush" : "Straight";
+    }
+    else if (pattern.includes(handPattern)) {
+        return (isColorSame(sumOfColors)) ? "Straight flush" : "Straight";
+    }
+}
+
+function chechIsPairThreeOrFour(cards) {
+    //cards.
+}
+
+function checkIsHighOrFlush(cards) {
+    //cards.
+}
 main();
