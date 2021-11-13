5e839cdf0b9444642bda9fa3da9cb6ca35a25436
alupinski <lbpro.arkadiusz.lupinksi@gmail.com>
2021-11-13 09:56:00 +0100

    ExamTask2 done

--- Homework_4/TaskNr5Exam2.js
+++ Homework_4/TaskNr5Exam2.js
@@ -1,7 +1,8 @@
 // losujesz 5 kart i wykrywasz najwyższy ukłąd
 // https://m.media-amazon.com/images/I/71xmLOiiywL._AC_SY741_.jpg
 
-const colorByValue = ['heart', 'diamond', 'club', 'spade']
+
+
 
 function getRandomIndex(max) {
     return Math.floor(Math.random() * (max)) + 1;
@@ -40,13 +41,10 @@ function compareObjects(a, b) {
 
 function main() {
     let pickedCards = pickFiveRandomCards(5);
-    checkHand(pickedCards);
+    console.log(checkHand(pickedCards));
 }
 function isColorSame(sumofColor) {
-    return Math.floor(sumofColor / 4) == 4 ||
-        Math.floor(sumofColor / 3) == 4 ||
-        Math.floor(sumofColor / 2) == 4 ||
-        Math.floor(sumofColor / 1) == 4;
+    return sumofColor % 5 == 0;
 }
 function chechIsPokerOrStrit(cards) {
     let pattern = '1,2,3,4,5,6,7,8,9,10,11,12,13,1';
@@ -59,19 +57,55 @@ function chechIsPokerOrStrit(cards) {
     });
 
     if (handPattern.includes('10,11,12,13') &&
-        handPattern.includes('0')) {
+        handPattern.includes('1')) {
         return (isColorSame(sumOfColors)) ? "Royal flush" : "Straight";
     }
     else if (pattern.includes(handPattern)) {
         return (isColorSame(sumOfColors)) ? "Straight flush" : "Straight";
     }
+    else return null;
 }
 
 function chechIsPairThreeOrFour(cards) {
-    //cards.
+    let arrayHand = [];
+    let newSetLength = 0;
+    cards.forEach(s=>{
+        if(arrayHand[s.value] )
+        {
+            arrayHand[s.value] +=1
+        } else
+        {
+            arrayHand[s.value] =1;
+            newSetLength++;
+        } 
+    });
+
+    
+    if(newSetLength ==4){
+        return 'One pair';
+    }
+    if(newSetLength==3){
+        if(arrayHand.indexOf(3)!=-1&& arrayHand.indexOf(2)!=-1)
+            return 'Three of a kind';
+        return 'Two pair';
+    }
+    if(newSetLength==2){
+        if(arrayHand.indexOf(4)!=-1)
+            return 'Four of a kind';
+        return 'Full house';
+    }
+    return null;
 }
 
 function checkIsHighOrFlush(cards) {
-    //cards.
+    let sumOfColors = 0;
+
+    cards.forEach(element => {
+        sumOfColors += element.color;
+    });
+    if(isColorSame(sumOfColors)){
+        return 'Flush';
+    }
+    return 'High card';
 }
 main();
