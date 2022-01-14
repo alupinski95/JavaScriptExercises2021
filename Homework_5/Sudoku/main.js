6d5c2339fd4d13d3e752ade77f8bdeb70f22e5a3
Arkadiusz Łupiński <a.lupinski@gmail.com>
2022-01-14 23:40:05 +0100

    New idea

--- Homework_5/Sudoku/main.js
+++ Homework_5/Sudoku/main.js
@@ -1,25 +1,47 @@
 
+// let sudokuTemp = [
+//     [7, 0, 4, 8, 0, 0, 3, 0, 1],
+//     [8, 2, 0, 5, 0, 0, 0, 4, 0],
+//     [0, 0, 9, 4, 3, 0, 5, 0, 0],
+//     [3, 1, 0, 0, 0, 0, 8, 0, 7],
+//     [0, 8, 0, 0, 0, 0, 0, 1, 0],
+//     [9, 0, 7, 0, 0, 0, 0, 3, 2],
+//     [0, 0, 6, 0, 1, 5, 4, 0, 0],
+//     [0, 7, 0, 0, 0, 9, 0, 6, 5],
+//     [5, 0, 8, 0, 0, 2, 1, 0, 3]
+// ]
 let sudokuTemp = [
-    [7, 0, 4, 8, 0, 0, 3, 0, 1],
-    [8, 2, 0, 5, 0, 0, 0, 4, 0],
-    [0, 0, 9, 4, 3, 0, 5, 0, 0],
-    [3, 1, 0, 0, 0, 0, 8, 0, 7],
-    [0, 8, 0, 0, 0, 0, 0, 1, 0],
-    [9, 0, 7, 0, 0, 0, 0, 3, 2],
-    [0, 0, 6, 0, 1, 5, 4, 0, 0],
-    [0, 7, 0, 0, 0, 9, 0, 6, 5],
-    [5, 0, 8, 0, 0, 2, 1, 0, 3]
+    [2, 6, 0, 0, 0, 0, 0, 0, 0],
+    [0, 8, 0, 0, 0, 9, 6, 0, 0],
+    [0, 0, 0, 0, 5, 0, 0, 0, 0],
+    [0, 9, 4, 3, 0, 0, 5, 0, 0],
+    [0, 0, 2, 0, 7, 0, 0, 0, 0],
+    [0, 5, 0, 0, 0, 0, 8, 0, 4],
+    [0, 3, 5, 8, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 3, 0, 1],
+    [7, 0, 0, 0, 6, 0, 0, 0, 0]
 ]
 const allPossibilities = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 let possibleNumbersToPutInCell = Array.from(Array(9), () => new Array(9));
-let possibleNumbersToPutInSquare =[];
+let possibleNumbersToPutInSquare = [];
 let numbersToFind = 0;
 
 function main() {
-    resloweSudoku(sudokuTemp,true);
+    resolveSudoku(sudokuTemp, true);
 
-    while(numbersToFind){
-        resloweSudoku(sudokuTemp,false);
+    while (numbersToFind) {
+        resolveSudoku(sudokuTemp, false);
+        console.log(numbersToFind);
+        console.log("---------------------------");
+        console.log(sudokuTemp[0]);
+        console.log(sudokuTemp[1]);
+        console.log(sudokuTemp[2]);
+        console.log(sudokuTemp[3]);
+        console.log(sudokuTemp[4]);
+        console.log(sudokuTemp[5]);
+        console.log(sudokuTemp[6]);
+        console.log(sudokuTemp[7]);
+        console.log(sudokuTemp[8]);
     }
     console.log(sudokuTemp[0]);
     console.log(sudokuTemp[1]);
@@ -32,7 +54,7 @@ function main() {
     console.log(sudokuTemp[8]);
 }
 
-function resloweSudoku(array,isFirstLoop) {
+function resolveSudoku(array, isFirstLoop) {
     for (let x = 0; x < array.length; x++) {
         let actualLine = sudokuTemp[x];
         let actualSquare = [];
@@ -40,25 +62,33 @@ function resloweSudoku(array,isFirstLoop) {
             if (array[x][y] > 0) {
                 continue;
             }
-            if(isFirstLoop) numbersToFind++;
-            
-            if (isNewSquareNeed(x, y) || !isFirstLoop){
+            if (isFirstLoop) numbersToFind++;
+
+            if (isNewSquareNeed(x, y) || !isFirstLoop) {
                 actualSquare = getActualSquare(array, x, y);
             }
 
-            
+
 
             let actualColumn = getActualColumn(array, y);
 
             let diff = [...new Set([...actualLine, ...actualSquare, ...actualColumn])];
             diff = allPossibilities.filter(x => !diff.includes(x));
             possibleNumbersToPutInCell[x][y] = diff;
-            
+
 
             if (possibleNumbersToPutInCell[x][y] && possibleNumbersToPutInCell[x][y].length == 1) {
                 let value = possibleNumbersToPutInCell[x][y][0];
                 sudokuTemp[x][y] = value;
                 updatePossibleRows(x, y, value);
+            }
+            else if (!isFirstLoop) {
+                let one = findOnePosible(possibleNumbersToPutInCell[x]);
+                if (one != -1)
+                    console.log(one + "x: " + x + "y: " + y);
+                let oneCol = findOnePosible(getActualColumn(possibleNumbersToPutInCell, y));
+                if (oneCol != -1)
+                    console.log(oneCol + "x: " + x + "y: " + y);
             }
         }
 
@@ -84,8 +114,8 @@ function isNewSquareNeed(x, y) {
 }
 
 function getActualSquare(array, x, y) {
-    x= Math.floor(x/3)*3;
-    y= Math.floor(y/3)*3;
+    x = Math.floor(x / 3) * 3;
+    y = Math.floor(y / 3) * 3;
     return [
         array[x][y],
         array[x][y + 1],
@@ -106,18 +136,38 @@ function updatePossibleRows(x, y, value) {
 }
 
 function removeFromPosibleSet(x, y, value) {
-    for(let i=0;i<9;i++){
-        if(possibleNumbersToPutInCell[i][y]){
+    for (let i = 0; i < 9; i++) {
+        if (possibleNumbersToPutInCell[i][y]) {
             let index = possibleNumbersToPutInCell[i][y].indexOf(value);
             if (index != -1)
                 possibleNumbersToPutInCell[i][y].splice(index, 1);
-        } 
-        if(possibleNumbersToPutInCell[x][i]){//col
+        }
+        if (possibleNumbersToPutInCell[x][i]) {//col
             let index = possibleNumbersToPutInCell[x][i].indexOf(value);
             if (index != -1)
                 possibleNumbersToPutInCell[x][i].splice(index, 1);
-        } 
+        }
     }
+    let squareX = Math.floor(x / 3) * 3;
+    let squareY = Math.floor(y / 3) * 3;
+    for (let xq = squareX; xq < squareX + 3; xq++) {
+        for (let yq = squareY; yq < squareY + 3; yq++) {
+            if (possibleNumbersToPutInCell[xq][yq]) {
+                let index = possibleNumbersToPutInCell[xq][yq].indexOf(value);
+                if (index != -1)
+                    possibleNumbersToPutInCell[xq][yq].splice(index, 1);
+            }
+        }
+    }
+}
+
+function findOnePosible(array) {
+    let helpArr = [];
+    let setFromArray = helpArr.concat(...array);
+    setFromArray.forEach(e => {
+        helpArr[e] ? helpArr[e] += 1 : helpArr[e] = 1;
+    });
+    return helpArr.findIndex(el => el == 1);
 }
 
 main();
--- Homework_5/TaskNr1.js
+++ Homework_5/TaskNr1.js
@@ -7,3 +7,38 @@
 //         e. 3 Kills 5
 //         f. 7 Kills 3
 //         g. 7 Remains alive
+
+let array = [];
+function createArray(lenght) {
+    for (let index = 0; index < lenght; index++) {
+        array[index] = index + 1;
+    }
+}
+function josephusProblem(personCounter) {
+    let arrayHelper = [];
+    for (let index = 0; index < personCounter; index += 2) {
+        if (!array[index + 1]) {
+            arrayHelper.unshift(array[index])
+        }
+        else {
+            console.log(array[index] + " kills " + array[index + 1]);
+            arrayHelper.push(array[index]);
+        }
+    }
+    array = [];
+    array.push(...arrayHelper);
+}
+
+
+
+
+
+function main(personCounter, step) {
+    createArray(personCounter);
+    while (array.length != 1)
+        josephusProblem(array.length);
+
+    console.log(array[0] + " Remains alive");
+
+}
+main(8, 2);
